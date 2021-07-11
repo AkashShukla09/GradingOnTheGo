7e393bed23b77f8c7c1421580564e48d3a31b4b9
AkashShukla09 <68428613+AkashShukla09@users.noreply.github.com>
2021-07-12 03:58:41 +0530

    file arrangement

--- tablestudent.php
+++ tablestudent.php
@@ -6,7 +6,7 @@ if (!$con)
 }
 $db=mysqli_select_db($con,"osp");
 
-$sql = "CREATE table student (name varchar(25), email varchar(30), campus varchar(10))";
+$sql = "CREATE table student (name varchar(25), reg varchar(9), email varchar(30))";
 
 
 if (mysqli_query($con, $sql) )
